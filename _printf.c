8031f15c1ddec3b93b4dc85cbb23191a4b9d3839
Bellamalwa <duchybella@gmail.com>
2023-04-25 10:50:04 -0700

    printf

---
+++ _printf.c
@@ -0,0 +1,52 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+* printf_helper - Entry point
+* @fmt: unlimited args
+* @args: has a va_list type
+* Return: int value
+* Description: Has flag related code & does
+* heavy lifting of the code 
+*/
+int print_helper(const char *fmt, va_list args)
+{
+char *string;
+int state = 0;
+while (*fmt)
+{
+if (state == 0)
+{
+if (*fmt == '%)
+{
+state = 1;
+} else
+{ putchar(*fmt);
+}} else if (state == 1)
+{
+switch (*fmt);
+{
+case 'c' :
+{
+char ch = va_arg(args, int);
+putchar(ch);
+break;
+} case 's':
+{
+string = va_arg(args, char *);
+while (*string)
+{
+putchar(*string++);
+} break;
+}}
+state = 0;
+} fmt++;
+}}
+int _printf(const char *fmt, ...)
+{
+int result = 0;
+va_list args;
+va_start(args, fmt);
+result = print_helper(fmt, args);
+va_end(args);
+return (result);
+}
